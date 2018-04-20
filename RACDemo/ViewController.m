e510ef6ee3c60f4293272258f8027e0d64b2d961
孟现进 <915719155@qq.com>
2018-04-20 08:30:18 +0800

    11

--- RACDemo/ViewController.m
+++ RACDemo/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 //导入RAC框架
 #import "ReactiveObjC.h"
+#import <ReactiveObjC/RACReturnSignal.h>
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
 
 @end
 
@@ -19,18 +22,27 @@
     [super viewDidLoad];
 //    信号的三步骤：创建信号、订阅信号、发送信号
     //创建信号（刚创建时，未被订阅，称为冷信号）
-    RACSignal *singal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {//subscriber订阅者
-        //发送信号
-        [subscriber sendNext:@"This is RAC"];
-        return nil;
-    }];
-    //    订阅信号
-    [singal subscribeNext:^(id  _Nullable x) {//x信号的内容
+//    RACSignal *singal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {//subscriber订阅者
+//        //发送信号
+//        [subscriber sendNext:@"This is RAC"];
+//        return nil;
+//    }];
+//    //    订阅信号
+//    [singal subscribeNext:^(id  _Nullable x) {//x信号的内容
+//        NSLog(@"%@",x);
+//    }];
+//    [[_textfield.rac_textSignal flattenMap:^__kindof RACSignal * _Nullable(NSString * _Nullable value) {
+//        return [RACReturnSignal return:[NSString stringWithFormat:@"信号内容：%@",value]];
+//    }]subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"%@",x);
+//    }];
+    
+    [[_textfield.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
+        return  [NSString stringWithFormat:@"信号内容：%@",value];
+    }]subscribeNext:^(id  _Nullable x) {
+        
         NSLog(@"%@",x);
     }];
-
-    
-    
     
 }
 
